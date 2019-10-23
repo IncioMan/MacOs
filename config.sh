f7225a1c0e09f965753dbe3411115a3ef2cbba6f
Alex Incerti <alexincerti@Alexs-MBP.homenet.telecomitalia.it>
2019-10-23 09:27:57 +0200

    initial setup

---
+++ config.sh
@@ -0,0 +1,5 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew cask install visual-studio-code
+brew cask install google-chrome
+brew cask install spotify
+brew install python3
