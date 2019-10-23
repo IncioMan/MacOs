c78fd63ed0226def1d5b69e7a5106994df49df9e
Alex Incerti <alexincerti@Alexs-MBP.homenet.telecomitalia.it>
2019-10-23 09:55:01 +0200

    path in bash profile

--- config.sh
+++ config.sh
@@ -1,5 +1,21 @@
+# Variables
+SCRIPT_FOLDER=Code/Scripts
+
+# Init
+cd ~
+
+# Create folders
+mkdir -p $SCRIPT_FOLDER
+
+#Create files
+touch ~/.bash_profile
+
+#Config PATH
+echo "PATH=$PATH:$SCRIPT_FOLDER" >> ~/.bash_profile
+
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew cask install visual-studio-code
 brew cask install google-chrome
 brew cask install spotify
 brew install python3
+brew cask install google-backup-and-sync
