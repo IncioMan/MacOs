484a3bee3dbdbff909426e158bc77edcc37cf9f9
Alex Incerti <alexincerti@Alexs-MBP.homenet.telecomitalia.it>
2020-01-07 18:50:38 +0100

    virtual box

--- config.sh
+++ config.sh
@@ -13,9 +13,12 @@ touch ~/.bash_profile
 #Config PATH
 echo "PATH=$PATH:$SCRIPT_FOLDER" >> ~/.bash_profile
 
+# Install sw
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew cask install visual-studio-code
 brew cask install google-chrome
 brew cask install spotify
 brew install python3
 brew cask install google-backup-and-sync
+brew cask install telegram
+brew cask install virtualbox
